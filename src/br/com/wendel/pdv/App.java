01f34d6f29fa75326538ac1ce8960a143fc1aa58
Wendel Nunes <wendel.nunesdonizete@gmail.com>
2018-02-21 15:25:28 -0300

    Commit inicial

---
+++ src/br/com/wendel/pdv/App.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.wendel.pdv;
+
+import br.com.wendel.pdv.view.HomeView;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author INLOC01
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            java.awt.EventQueue.invokeLater(() -> {
+                new HomeView().setVisible(true);
+            });
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
